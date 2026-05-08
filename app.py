434676f04d702b92f1116caae416ba306b434495
Comunicando Igrejas <comunicandoigrejas@gmail.com>
2026-05-08 15:43:27 -0300

    Update app.py

--- app.py
+++ app.py
@@ -78,7 +78,7 @@ elif selected == "Cardápio":
     lista_doces = [
         {"nome": "Trufas", "preco": 4.00, "img": "assets/trufas1.png"},
         {"nome": "Cone Trufado", "preco": 8.00, "img": "assets/conetrufado1.png"},
-        {"nome": "Pão de Mel", "preco": 8.00, "img": "assets/paodemel1.png"}
+        {"nome": "Pão de Mel", "preco": 8.00, "img": "assets/paodemel1.png"},
         {"nome": "Kit 4 Trufas", "preco": 15.00, "img": "assets/trufas4unidades1.png"}
     ]
     for doce in lista_doces:
